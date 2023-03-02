41a6b4ca2b04fbc2eab49b214a3cc85de5c0ec60
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-03-01 20:31:57 -0800

    route test

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/api/ctgs', async (req, res) => {
+  try {
+    const CData = await Category.findAll({
+      include:[{ model: Product }] ,
+    });
+    res.status(200).json(CData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id',async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const cateData = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+
+    if (!cateData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
